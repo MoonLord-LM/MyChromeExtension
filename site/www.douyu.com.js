837a9e946a3bcc819b48eda6875feaae7c2a4bc1
月之领主LM <178910432@qq.com>
2020-06-06 23:39:58 +0800

    Update

---
+++ site/www.douyu.com.js
@@ -0,0 +1,14 @@
+console.log('MyChromeExtension : www.douyu.com.js is loaded');
+
+// 自动发送弹幕
+var send_chat = function () {
+    var chat_text = document.querySelector('textarea.ChatSend-txt');
+    var chat_button = document.querySelector('div.ChatSend-button');
+    if(chat_text !== null && chat_button !== null ){
+        chat_text.value = '？？？ ？？？ ？？？ ？？？ ？？？ ？？？';
+        chat_button.click();
+    }
+    //console.log('send_chat is running ...');
+}
+send_chat();
+setInterval(send_chat, 3000);
