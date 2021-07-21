b6888ad7464d4ea51fb2eebc2d77fa4442eb3834
MoonLord-LM <178910432@qq.com>
2021-07-21 18:51:50 +0800

    Update

--- site/all.js
+++ site/all.js
@@ -1,4 +1,24 @@
-console.log('MyChromeExtension : all.js is loaded');
+// 显示已加载的插件 JS 文件名
+{
+    var new_error_stack = (new Error).stack.split("\n");
+    if(new_error_stack.length >= 2) {
+        var stack0 = new_error_stack[0];
+        var stack1 = new_error_stack[1];
+        if(stack0 === "Error") {
+            var file_name_line = stack1.trim();
+            if(file_name_line.startsWith("at ")) {
+                file_name_line = file_name_line.substr(3);
+                if(file_name_line.indexOf(":") !== -1) {
+                    file_name_line = file_name_line.substr(0, file_name_line.lastIndexOf(":"));
+                    if(file_name_line.indexOf(":") !== -1) {
+                        var js_file_name = file_name_line.substr(0, file_name_line.lastIndexOf(":"));
+                        console.log('MyChromeExtension load js: ' + js_file_name);
+                    }
+                }
+            }
+        }
+    }
+};
 
 // Console 显示自动输入的密码
 var passwords_logged = [];
@@ -16,8 +36,8 @@ var log_password = function () {
 log_password();
 setInterval(log_password, 3000);
 
-chrome.storage.sync.set({"foo":"bar"}, function() { console.log("saved ok"); } );
-chrome.storage.sync.get("foo", function(data) { console.log(data); } );
+chrome.storage.sync.set({"foo":"bar"}, function() { console.log("set foo: bar"); } );
+chrome.storage.sync.get("foo", function(data) { console.log("get foo: " + JSON.stringify(data)); } );
 
 // 鼠标悬停显示源码
 /*
