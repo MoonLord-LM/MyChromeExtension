166fa83bbcf1e68d8144ac9ed9834be80d6283f8
MoonLord-LM <178910432@qq.com>
2021-07-30 10:56:41 +0800

    Update

--- site/all.js
+++ site/all.js
@@ -2,21 +2,21 @@
 var my_get_loaded_js = function () {
     var error_stack = (new Error).stack.split("\n");
     var error_position = error_stack[error_stack.length - 1].trim();
-    if (error_position.startsWith("at ")) {
+    if (error_position.startsWith('at ')) {
         error_position = error_position.substr(3);
     }
-    if (error_position.indexOf("(") !== -1) {
-        error_position = error_position.substr(error_position.indexOf("(") + 1);
+    if (error_position.indexOf('(') !== -1) {
+        error_position = error_position.substr(error_position.indexOf('(') + 1);
     }
-    if (error_position.indexOf(":") !== -1) {
-        error_position = error_position.substr(0, error_position.lastIndexOf(":"));
+    if (error_position.indexOf(':') !== -1) {
+        error_position = error_position.substr(0, error_position.lastIndexOf(':'));
     }
-    if (error_position.indexOf(":") !== -1) {
-        error_position = error_position.substr(0, error_position.lastIndexOf(":"));
+    if (error_position.indexOf(':') !== -1) {
+        error_position = error_position.substr(0, error_position.lastIndexOf(':'));
     }
-    if (!error_position.startsWith("chrome-extension://") || !error_position.endsWith(".js")) {
-        if(error_position !== "<anonymous>") {
-            console.error("MyChromeExtension show_loaded_js failed, error_position: " + error_position);
+    if (!error_position.startsWith('chrome-extension://') || !error_position.endsWith('.js')) {
+        if(error_position !== '<anonymous>') {
+            console.error('MyChromeExtension show_loaded_js failed, error_position: ' + error_position);
         }
     }
     return error_position;
@@ -30,16 +30,16 @@ my_show_loaded_js();
 // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/storage/StorageArea
 var my_show_storage_usage = function () {
     chrome.storage.sync.get(null, function(data) {
-        console.log("MyChromeExtension sync storage: " + JSON.stringify(data));
+        console.log('MyChromeExtension sync storage: ' + JSON.stringify(data));
     });
     chrome.storage.sync.getBytesInUse(null, function(data) {
-        console.log("MyChromeExtension sync storage usage: " + data + " Bytes");
+        console.log('MyChromeExtension sync storage usage: ' + data + ' Bytes');
     });
     chrome.storage.local.get(null, function(data) {
-        console.log("MyChromeExtension storage: " + JSON.stringify(data));
+        console.log('MyChromeExtension storage: ' + JSON.stringify(data));
     });
     chrome.storage.local.getBytesInUse(null, function(data) {
-        console.log("MyChromeExtension storage usage: " + data + " Bytes");
+        console.log('MyChromeExtension storage usage: ' + data + ' Bytes');
     });
 };
 my_show_storage_usage();
@@ -47,29 +47,29 @@ my_show_storage_usage();
 var my_set_js_setting = function (key, value) {
     var data = {};
     data[key] = value;
-    console.log("MyChromeExtension my_set_js_setting: " + JSON.stringify(data));
+    console.log('MyChromeExtension my_set_js_setting: ' + JSON.stringify(data));
     chrome.storage.sync.set(data, function() { });
     chrome.storage.local.set(data, function() { });
 };
-my_set_js_setting("MyChromeExtension","MyChromeExtension");
+my_set_js_setting('MyChromeExtension','MyChromeExtension');
 
 var my_get_js_setting = function (key) {
-    console.log("MyChromeExtension my_get_js_setting: " + key);
+    console.log('MyChromeExtension my_get_js_setting: ' + key);
     chrome.storage.local.get(key, function(data) {
         console.log(JSON.stringify(data));
         return data;
     });
 };
-my_set_js_setting("MyChromeExtension","MyChromeExtension");
-console.log("my_get_js_setting: " + my_get_js_setting("MyChromeExtension"));
+my_set_js_setting('MyChromeExtension','MyChromeExtension');
+console.log('my_get_js_setting: ' + my_get_js_setting('MyChromeExtension'));
 
 chrome.storage.sync.clear();
-var my_chrome_extension_setting1 = {"foo1":"bar1","foo2":"bar2"};
-var my_chrome_extension_setting2 = {"foo3":"bar3","foo4":"bar4"};
+var my_chrome_extension_setting1 = {'foo1':'bar1','foo2':'bar2'};
+var my_chrome_extension_setting2 = {'foo3':'bar3','foo4':'bar4'};
 chrome.storage.sync.set(my_chrome_extension_setting1, function() { console.log(my_chrome_extension_setting1); } );
 chrome.storage.sync.set(my_chrome_extension_setting2, function() { console.log(my_chrome_extension_setting2); } );
-chrome.storage.sync.get("foo1", function(data) { console.log(JSON.stringify(data)); } );
-chrome.storage.sync.get("foo2", function(data) { console.log(JSON.stringify(data)); } );
+chrome.storage.sync.get('foo1', function(data) { console.log(JSON.stringify(data)); } );
+chrome.storage.sync.get('foo2', function(data) { console.log(JSON.stringify(data)); } );
 
 // Console 显示自动输入的密码
 var passwords_logged = [];
@@ -78,7 +78,7 @@ var log_password = function () {
     for (i = 0; i < passwords.length; i++) {
         var password = passwords[i].value;
         if(password !== '' && passwords_logged.indexOf(password) === -1){
-            console.log("password : " + password);
+            console.log('password : ' + password);
             passwords_logged.push(password);
         }
     }
