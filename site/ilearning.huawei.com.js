a102ba47b7822dfa2843649ff0060816bc7187e6
MoonLord-LM <178910432@qq.com>
2022-12-26 18:50:06 +0800

    Update

--- site/ilearning.huawei.com.js
+++ site/ilearning.huawei.com.js
@@ -14,14 +14,15 @@ setInterval(disable_switch_detect, 3000);
 var allow_mouse_interact = function () {
     var exam_content = document.querySelector(".exam-content-container")
     if(exam_content !== null){
+        exam_content.className = '';
         exam_content.oncontextmenu = null;
-        var listeners = getEventListeners(exam_content)["contextmenu"];
+        var listeners = getEventListeners(exam_content)["contextmenu"]; // TODO
         for (i = 0; i < listeners.length; i++) {
             exam_content.removeEventListener("contextmenu", listeners[i].listener);
             console.log('allow_mouse_interact: ' + listeners[i].listener);
         }
         exam_content.onselectstart = null;
-        listeners = getEventListeners(exam_content)["selectstart"];
+        listeners = getEventListeners(exam_content)["selectstart"]; // TODO
         for (i = 0; i < listeners.length; i++) {
             exam_content.removeEventListener("selectstart", listeners[i].listener);
             console.log('allow_mouse_interact: ' + listeners[i].listener);
