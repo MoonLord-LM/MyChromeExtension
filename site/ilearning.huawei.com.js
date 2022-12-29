3d8d7bc5a5b1ff313a6a717d4a7dec580078e60a
MoonLord-LM <178910432@qq.com>
2022-12-29 18:07:38 +0800

    Update

--- site/ilearning.huawei.com.js
+++ site/ilearning.huawei.com.js
@@ -2,19 +2,16 @@ typeof (showLoadedFile) === 'function' && showLoadedFile();
 
 
 
-// 允许鼠标选中文字
+// 移除焦点监控，允许鼠标选中文字
 var script = `
 var allow_mouse_interact = function () {
-    var content1 = document.querySelector('.exam-content-container');
-    if (content1 !== null) {
-        content1.oncontextmenu = function () { };
-        content1.onselectstart = function () { };
-        content1.disableEventListeners('contextmenu');
-        content1.disableEventListeners('selectstart');
-    }
-    var content2 = document.querySelector('.exam-content-copy');
-    if (content2 !== null) {
-        content2.classList.remove('exam-content-copy');
+    document.disableEventListeners('contextmenu');
+    document.disableEventListeners('selectstart');
+    document.disableEventListeners('blur');
+
+    var content = document.querySelector('.exam-content-copy');
+    if (content !== null) {
+        content.classList.remove('exam-content-copy');
     }
     console.log('allow_mouse_interact is running ...');
 }
