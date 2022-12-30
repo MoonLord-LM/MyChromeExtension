b2da31ce1313c6b7d8029e0438ca86f31c36b39b
MoonLord-LM <178910432@qq.com>
2022-12-30 15:11:02 +0800

    Update

--- site/ilearning.huawei.com.js
+++ site/ilearning.huawei.com.js
@@ -2,21 +2,31 @@ typeof (showLoadedFile) === 'function' && showLoadedFile();
 
 
 
-// 移除焦点监控，允许鼠标选中文字
-var script = `
-var allow_mouse_interact = function () {
-    document.disableEventListeners('contextmenu');
-    document.disableEventListeners('selectstart');
-    document.disableEventListeners('blur');
-
-    var content = document.querySelector('.exam-content-copy');
-    if (content !== null) {
-        content.classList.remove('exam-content-copy');
-    }
-    console.log('allow_mouse_interact is running ...');
+// 移除鼠标焦点监控，允许鼠标选中文字
+// 失去焦点 onblur、右键 oncontextmenu、文字选择 onselectstart、复制 oncopy
+/*
+.exam-content-copy {
+    user-select: none;
+    -moz-user-select: none;
+    -webkit-user-select: none;
+    -ms-user-select: none;
 }
-allow_mouse_interact();
-setInterval(allow_mouse_interact, 3000);
+*/
+var script = `
+    var allow_mouse_interact = function () {
+        document.disableEventListeners('blur');
+        document.disableEventListeners('contextmenu');
+        document.disableEventListeners('selectstart');
+        document.disableEventListeners('copy');
+
+        var content = document.querySelector('.exam-content-copy');
+        if (content !== null) {
+            content.classList.remove('exam-content-copy');
+        }
+        console.log('allow_mouse_interact is running ...');
+    }
+    allow_mouse_interact();
+    setInterval(allow_mouse_interact, 3000);
 `;
 
 document.dispatchEvent(new CustomEvent('executeScript', { detail: script }));
