4d614467584256096c4844fc2c73045aa23a4153
MoonLord-LM <moonlord@msn.cn>
2023-05-25 21:39:56 +0800

    Update hr.newcoder.com.js

--- site/hr.newcoder.com.js
+++ site/hr.newcoder.com.js
@@ -8,27 +8,34 @@ typeof (showLoadedFile) === 'function' && showLoadedFile();
 
 
 if(window.location.href.startsWith('https://hr.nowcoder.com/console/paper/candidate/result')){
-    document.querySelector('.test-report-right').style.display = 'none';
-    document.querySelector('.test-report-left-tab').style.display = 'none';
-    document.querySelectorAll('.question-header').forEach (function (object) {
-        object.querySelector('.right').style.display = 'none';
+    // 避免保存网页时泄露信息
+    history.pushState(null, null, window.location.href.split('?')[0]);
+    setTimeout(function(){
+        document.getElementsByTagName("title")[0].innerText = '牛客网考试编程题';
+    }, 1000);
+    // 删除多余元素
+    document.querySelectorAll('.test-report-right , .test-report-left-tab').forEach (function (object) {
+        object.parentNode.removeChild(object);
+    });
+    document.querySelectorAll('.question-header .right').forEach (function (object) {
+        object.parentNode.removeChild(object);
     });
     document.querySelectorAll('.code-fragment-wrap').forEach (function (object) {
-        object.style.display = 'none';
+        object.parentNode.removeChild(object);
     });
     document.querySelectorAll('.answer-main').forEach (function (object) {
-        object.style.display = 'none';
+        object.parentNode.removeChild(object);
     });
     document.querySelectorAll('.examples-header .switch').forEach (function (object) {
         if(object.innerText === ' 展开'){
             object.click();
-            object.style.display = 'none';
+            object.parentNode.removeChild(object);
         }
         else if(object.innerText === ' 收起'){
-            object.style.display = 'none';
+            object.parentNode.removeChild(object);
         }
         else {
-            console.log(object.innerText)
+            console.log(object.innerText);
         }
     });
 }
