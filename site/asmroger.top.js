b99a9e1cc5774055e4f625b9de74e7061039e467
MoonLord-LM <me@moonlord.cn>
2025-03-31 23:44:22 +0800

    Update

--- site/asmroger.top.js
+++ site/asmroger.top.js
@@ -3,17 +3,23 @@ typeof (showLoadedFile) === 'function' && showLoadedFile();
 
 // streamtape.com
 var videoElement = document.querySelector('iframe[data-src*="streamtape.com"]');
-
-// bigwarp.io
-if (videoElement != null) {
-    videoElement = document.querySelector('iframe[data-src*="bigwarp.io"]');
-}
-
 if (videoElement != null) {
     var dataSrc = videoElement.getAttribute('data-src');
     console.log('dataSrc: ' + dataSrc);
     var updatedLink = dataSrc;
     updatedLink = updatedLink.replace(/streamtape.com/g, 'watchadsontape.com');
+    console.log('updatedLink: ' + updatedLink);
+    window.location.href = updatedLink;
+}
+
+// bigwarp.io
+if (videoElement === null) {
+    videoElement = document.querySelector('iframe[src*="bigwarp.io"]');
+}
+if (videoElement != null) {
+    var dataSrc = videoElement.getAttribute('src');
+    console.log('dataSrc: ' + dataSrc);
+    var updatedLink = dataSrc;
     updatedLink = updatedLink.replace(/bigwarp.io/g, 'bigwarp.art');
     console.log('updatedLink: ' + updatedLink);
     window.location.href = updatedLink;
