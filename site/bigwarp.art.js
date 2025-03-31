67424f43128c1a396b268c06174b66765c98b5f3
MoonLord-LM <me@moonlord.cn>
2025-03-31 23:50:38 +0800

    Update

--- site/bigwarp.art.js
+++ site/bigwarp.art.js
@@ -1,11 +1,13 @@
 typeof (showLoadedFile) === 'function' && showLoadedFile();
 
 
-var video = document.querySelector('video');
-if (video != null) {
-    var link = video.src;
-    console.log('link: ' + link);
-    window.location.href = link;
-}
+setInterval(function() {
+    video = document.querySelector('video');
+    if (video != null) {
+        var link = video.src;
+        console.log('link: ' + link);
+        window.location.href = link;
+    }
+}, 3000);
 
 
